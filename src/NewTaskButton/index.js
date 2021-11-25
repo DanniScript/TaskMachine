79f61a2a32a24f29b280d6898f2e5eebe67fa959
Daniel Rincón Hdez <danielrinconher@gmail.com>
2021-11-24 22:57:19 -0500

    Fix/Update: Add task button resposive   - Fix button styles to add task

--- src/NewTaskButton/index.js
+++ src/NewTaskButton/index.js
@@ -2,8 +2,10 @@ import './style.css'
 
 function NewTaskButton() {
   return (
-    <div className="new-task-button">
-      <button>Add Task</button>
+    <div className="new-task-button__container">
+      <div className="new-task-button">
+        <button>Add Task</button>
+      </div>
     </div>
   )
 }
